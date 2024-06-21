afd0acea9a026a26df66e9e1ae2d9ca00e0ae69a
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2024-06-20 21:15:05 -0400

    Complete day 1 solution

--- 2023/01/01-solution.py
+++ 2023/01/01-solution.py
@@ -10,11 +10,10 @@ with open("input", "r") as f:
 # Combine the first digit and last digit to form a two-digit number
 # In some cases, the _only_ number is both the first and last number.
 def part1(input):
-    firstnum_rxp = re.compile(r"^.*?(\d).*$")
-    firstnums = [re.match(firstnum_rxp, x).groups()[0] for x in input]
-    lastnum_rxp = re.compile(r"^.*(\d).*?$")
-    lastnums = [re.match(lastnum_rxp, x).groups()[0] for x in input]
-    numbers = [int(''.join(pair)) for pair in zip(firstnums, lastnums)]
+    # Remove all non-number characters
+    nums = [re.sub(r'\D', '', x) for x in input]
+    # Combine the first and last numbers (note that sometimes, these are the same)
+    numbers = [int(x[0] + x[-1]) for x in nums]
     return sum(numbers)
 
 part1(input)
@@ -22,32 +21,38 @@ part1(input)
 # Part 2:
 # Some of the numbers are spelled out (e.g., one, two, three). First, replace 
 # all of these with the corresponding number; then, redo.
-def replace_digit(s: str):
-    numstrings = {
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9"
-    }
-    result = s
+# NOTE: A challenge here is that you can sometimes have strings right next to 
+# each other; e.g., `oneight` needs to be converted to 18 to be parsed correctly.
+
+numstrings = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+}
+
+def get_number(s: str, numstrings):
+    rxp = r"(\d|" + '|'.join(numstrings.keys()) + r")"
+    result = re.findall(rxp, s)[0]
     for key, value in numstrings.items():
         result = result.replace(key, value)
     return result
 
-re.sub(r'[^0-9]', '', "string123")
-"string123".replace(re.compile('^(\d)'), '')
+def first_number(s):
+    return get_number(s, numstrings)
 
-str.maketrans({"xx": "doublex"})
-"12xx34".translate(str.maketrans({"xx": "xx"}))
+def last_number(s):
+    numstrings_rev = {key[::-1]: value for key, value in numstrings.items()}
+    return get_number(s[::-1], numstrings_rev)
 
-input[0]
-input_fixed[0]
-part1(input[0:1])
-part1(input_fixed[0:1])
-input_fixed = [replace_digit(x) for x in input]
-part1(input_fixed)
+def code(s):
+    return int(first_number(s) + last_number(s))
+
+numbers_part2 = (code(x) for x in input)
+# Part 2 solution:
+sum(numbers_part2)
