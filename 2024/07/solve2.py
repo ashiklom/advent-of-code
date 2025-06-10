119566d40d059046224fe59fcd067ccec5754b50
Alexey Shiklomanov <alexey.shiklomanov@nasa.gov>
2025-06-09 21:37:13 -0400

    solve 7.2

---
+++ 2024/07/solve2.py
@@ -0,0 +1,44 @@
+from operator import add, mul
+from itertools import product
+
+def concat(a, b):
+    return int(str(a)+str(b))
+
+with open("2024/07/input") as f:
+    lines = f.read().splitlines()
+
+def flist(n):
+    return product((add, mul), repeat=n)
+
+def flist2(n):
+    return product((add, mul, concat), repeat=n)
+
+def apply(fs, nums):
+    nc = nums.copy()
+    r = nc.pop(0)
+    for f, num in zip(fs, nc):
+        r = f(r, num)
+    return r
+
+def attempt(answer, nums):
+    for fs in flist(len(nums)-1):
+        r = apply(fs, nums)
+        if r == answer:
+            return r
+
+    for fs in flist2(len(nums)-1):
+        r = apply(fs, nums)
+        if r == answer:
+            return r
+
+    return 0
+
+result = 0
+
+for line in lines:
+    answer_str, numlist = line.split(":")
+    answer = int(answer_str)
+    nums = list(map(int, numlist.split()))
+    result += attempt(answer, nums)
+
+print(result)
