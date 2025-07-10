b8751c1f129634c22b658e1f4852dfecd6dd59e7
Alexey <alexey.shiklomanov@gmail.com>
2025-07-09 21:05:01 -0400

    Format. Refactor turn counting logic in gn.

--- 2024/16/solve1.py
+++ 2024/16/solve1.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
 
 
 def i2rc(i, ncol):
-    r = i // (ncol+1)
-    c = i - (ncol+1)*r
+    r = i // (ncol + 1)
+    c = i - (ncol + 1) * r
     return r, c
+
 
 # fname = "2024/16/test1"
 # fname = "2024/16/test2"
@@ -19,9 +21,71 @@ ncol = len(grid[0])
 
 sr, sc = i2rc(raw.find("S"), ncol)
 er, ec = i2rc(raw.find("E"), ncol)
-d0 = (0,1)
+d0 = (0, 1)
 
-def gn(r: int, c: int, direction: tuple[int,int]):
+
+def turns(dr: int, dc: int, going_r: int, going_c: int) -> int:
+    """
+    Minimum number of turns needed given a distance and a heading.
+    """
+    if dr == 0:
+        target_r = 0
+    else:
+        target_r = dr / abs(dr)
+
+    if dc == 0:
+        target_c = 0
+    else:
+        target_c = dc / abs(dc)
+
+    if dr == 0:
+        # Same row
+        if going_r != 0:
+            # Moving horizontally
+            if going_r == target_r:
+                # Same direction; no turns
+                return 0
+            # Opposite direction; 3 turns to turn around
+            else:
+                return 3
+        # Moving vertically
+        else:
+            if going_c == target_c:
+                # Right direction; only one turn
+                return 1
+            else:
+                # Wrong direction; 2 turns to turn around
+                return 2
+
+    elif dc == 0:
+        # Same logic, but for same column
+        if going_c != 0:
+            if going_c == target_c:
+                return 0
+            else:
+                return 3
+        else:
+            if going_r == target_r:
+                return 1
+            else:
+                return 2
+
+    # Different row and column
+    # 1 turn if heading in the right direction; 2 turns if in the wrong direction
+    else:
+        if going_r == 0:
+            if going_c == target_c:
+                return 1
+            else:
+                return 2
+        if going_c == 0:
+            if going_r == target_r:
+                return 1
+            else:
+                return 2
+
+
+def gn(r: int, c: int, direction: tuple[int, int]):
     """
     A* heuristic for route next steps.
     """
@@ -31,18 +95,7 @@ def gn(r: int, c: int, direction: tuple[int,int]):
     if dr == 0 and dc == 0:
         # At destination. gn = 0
         return 0
-    dra = abs(dr)
-    dca = abs(dc)
-    # Minimum number of turns required
-    turns = 0
-    if dr != 0 and dc != 0:
-        turns += direction[0] != dr/dra
-        turns += direction[1] != dc/dca
-    elif dr == 0:
-        turns += 3 * (direction[1] != dc/dca)
-    elif dc == 0:
-        turns += 3 * (direction[0] != dr/dra)
-    return dra + dca + turns*1000
+    return abs(dr) + abs(dc) + turns(dr, dc, direction[0], direction[1]) * 1000
 
 
 class Cell:
@@ -55,29 +108,34 @@ class Cell:
         self.astar = self.fn + self.gn
 
     def __repr__(self) -> str:
-        return str({
-            "rc": (self.r, self.c),
-            "dir": self.direction,
-            "fn": self.fn,
-            "gn": self.gn,
-            "astar": self.astar
-        })
+        return str(
+            {
+                "rc": (self.r, self.c),
+                "dir": self.direction,
+                "fn": self.fn,
+                "gn": self.gn,
+                "astar": self.astar,
+            }
+        )
+
 
 cells = [Cell(sr, sc, d0, 0)]
 edges = [Cell(sr, sc, d0, 0)]
 
+
 def coords_checked(coord, cells):
     coords = {(c.r, c.c) for c in cells}
-    return (coord in coords)
+    return coord in coords
+
 
 def options(cell: Cell, cells: list[Cell]) -> list[Cell]:
     """
     List of valid routes at a location.
     """
     opts = []
-    for dr, dc in ((0,1), (1,0), (0,-1), (-1,0)):
-        rr, cc = (cell.r+dr, cell.c+dc)
-        if coords_checked((rr,cc), cells):
+    for dr, dc in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+        rr, cc = (cell.r + dr, cell.c + dc)
+        if coords_checked((rr, cc), cells):
             continue
         if not 0 <= rr < nrow:
             continue
@@ -87,32 +145,12 @@ def options(cell: Cell, cells: list[Cell]) -> list[Cell]:
         if obj == "#":
             continue
         fn = cell.fn + 1
-        if (dr,dc) != cell.direction:
+        if (dr, dc) != cell.direction:
             fn += 1000
-        new = Cell(rr, cc, (dr,dc), fn)
+        new = Cell(rr, cc, (dr, dc), fn)
         opts.append(new)
     return opts
 
-# def draw(route: Route):
-#     g = deepcopy(grid)
-#     for cell in route.cells:
-#         r = cell.r
-#         c = cell.c
-#         match cell.direction:
-#             case (0,1):
-#                 char = ">"
-#             case (1,0):
-#                 char = "v"
-#             case (0,-1):
-#                 char = "<"
-#             case (-1,0):
-#                 char = "^"
-#             case _:
-#                 raise ValueError(f"Invalid direction: {cell.direction}")
-#         if g[r][c] not in ("S", "E"):
-#             g[r][c] = char
-#     print("\n".join("".join(row) for row in g))
-#     print("-"*12)
 
 cell = None
 
