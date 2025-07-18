3d9105d1068348698d1c6f00bac06d9e2be597cf
Alexey <alexey.shiklomanov@gmail.com>
2025-07-18 16:45:20 -0400

    Also use helpers in 16.1

--- 2024/16/solve2.py
+++ 2024/16/solve2.py
@@ -5,6 +5,7 @@ from copy import deepcopy
 # fname = "2024/16/test1"
 # astar_target = 7036
 # answer = 45
+from helpers import turns, i2rc
 
 # fname = "2024/16/test2"
 # astar_target = 11048
@@ -20,11 +21,6 @@ rows = raw.split("\n")
 grid = list(map(list, rows))
 nrow = len(grid)
 ncol = len(grid[0])
-
-def i2rc(i, ncol):
-    r = i // (ncol + 1)
-    c = i - (ncol + 1) * r
-    return r, c
 
 sr, sc = i2rc(raw.find("S"), ncol)
 er, ec = i2rc(raw.find("E"), ncol)
@@ -46,63 +42,7 @@ for r, row in enumerate(grid):
 
 type coord = tuple[int, int]
 
-def turns(dr: int, dc: int, going_r: int, going_c: int) -> int:
-    """
-    Minimum number of turns needed given a distance and a heading.
-    """
-    if dr == 0:
-        target_r = 0
-    else:
-        target_r = int(dr / abs(dr))
-
-    if dc == 0:
-        target_c = 0
-    else:
-        target_c = int(dc / abs(dc))
-
-    if dr == 0:
-        # Same row
-        if going_r == 0:
-            # Moving horizontally
-            if going_c == target_c:
-                # Same direction; no turns
-                return 0
-            # Opposite direction; 3 turns to turn around
-            else:
-                return 3
-        # Moving vertically
-        else:
-            # Going vertical. Need to go horizontal. Only need 1 turn.
-            return 1
-
-    elif dc == 0:
-        # Same logic, but for same column
-        if going_c == 0:
-            if going_r == target_r:
-                return 0
-            else:
-                return 3
-        else:
-            return 1
-
-    # Different row and column
-    # 1 turn if heading in the right direction; 2 turns if in the wrong direction
-    else:
-        if going_r == 0:
-            if going_c == target_c:
-                return 1
-            else:
-                return 2
-        if going_c == 0:
-            if going_r == target_r:
-                return 1
-            else:
-                return 2
-
-    raise ValueError("Can't calcluate turns")
-
-
-def gn(r: int, c: int, direction: tuple[int, int], er: int, ec: int):
+def gn(r: int, c: int, direction: coord, er: int, ec: int):
     """
     A* heuristic for route next steps.
     """
@@ -116,14 +56,14 @@ def gn(r: int, c: int, direction: tuple[int, int], er: int, ec: int):
 
 class Cell:
     def __init__(
-        self, r: int, c: int, direction: coord, fn: int, route: list = []
+        self, r: int, c: int, direction: coord, fn: int, parent: "Cell|None" = None
     ) -> None:
         self.r = r
         self.c = c
         self.direction = direction
         self.fn = fn
         self.astar = 0
-        self.route = route + [(self.r, self.c)]
+        self.parent = parent
         self.opts = []
 
     def get_options(self, cells: dict[coord, "Cell"], end: coord) -> None:
@@ -132,15 +72,12 @@ class Cell:
         """
         for dr, dc in ((0, 1), (1, 0), (0, -1), (-1, 0)):
             rr, cc = (self.r + dr, self.c + dc)
+            if self.parent and (rr, cc) == self.parent.coord:
+                continue
+            if (rr, cc) in cells:
+                continue
             obj = grid[rr][cc]
             if obj == "#":
-                continue
-            try:
-                if (rr, cc) == self.route[-2]:
-                    continue
-            except IndexError:
-                pass
-            if (rr, cc) in cells:
                 continue
             fn = self.fn + 1
             if (dr, dc) != self.direction:
@@ -150,9 +87,13 @@ class Cell:
             astar = fn + gn(rr, cc, (dr, dc), end[0], end[1])
             if astar > astar_target:
                 continue
-            new = Cell(rr, cc, (dr, dc), fn, route=self.route)
+            new = Cell(rr, cc, (dr, dc), fn, parent=self)
             new.astar = astar
             self.opts.append(new)
+
+    @property
+    def coord(self) -> coord:
+        return (self.r, self.c)
 
     def __repr__(self) -> str:
         return str(
@@ -163,6 +104,26 @@ class Cell:
             }
         )
 
+
+def reconstruct_route(cell: "Cell"):
+    route = [cell.coord]
+    while cell.parent:
+        cell = cell.parent
+        route.append(cell.coord)
+    return route
+
+def draw(grid: list[list[str]], cells: set[coord], alt: set[coord] = set()):
+    g = deepcopy(grid)
+    for r, c in cells:
+        g[r][c] = "x"
+    for r, c in alt:
+        g[r][c] = "O"
+    # g[sr][sc] = "S"
+    # g[er][ec] = "E"
+    result = "\n".join("".join(row) for row in g)
+    print(result)
+
+
 def solve(start: coord, end: coord, d0: coord, fn: int = 0):
     cell = Cell(start[0], start[1], d0, fn)
     cells = {start: cell}
@@ -170,14 +131,11 @@ def solve(start: coord, end: coord, d0: coord, fn: int = 0):
     while edges:
         cell = edges.pop()
         if ((cell.r == end[0]) and (cell.c == end[1])):
-            start_cell = cells[cell.route[0]]
-            return (start_cell, cell)
+            return cell
         cell.get_options(cells, end)
         if cell.opts:
             edges += cell.opts
             edges.sort(key=lambda x: x.astar, reverse=True)
-        cells[(cell.r, cell.c)] = cell
-    return None
 
 test = solve((sr, sc), (er, ec), (0, 1))
 
@@ -196,16 +154,6 @@ def solve_fork(fork: coord):
     result.update(set(to_end.route))
     result.update(set(solve_start[1].route))
 
-def draw(grid: list[list[str]], cells: set[coord], alt: set[coord] = set()):
-    g = deepcopy(grid)
-    for r, c in cells:
-        g[r][c] = "x"
-    for r, c in alt:
-        g[r][c] = "O"
-    g[sr][sc] = "S"
-    g[er][ec] = "E"
-    result = "\n".join("".join(row) for row in g)
-    print(result)
 
 for i, fork in enumerate(forks):
     if i % 100 == 0:
