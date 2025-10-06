8df5185c3e844e0ebae456ab45d251485c0c85d6
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2025-10-05 21:58:18 -0400

    2023.03.1 solve

--- 2023/03/solve1.hs
+++ 2023/03/solve1.hs
@@ -4,17 +4,22 @@ import Data.List
 import Data.Map (Map)
 import qualified Data.Map as Map
 
-getPred :: (Char -> Bool) -> Int -> String -> [(Int, String)]
-getPred _ _ "" = []
-getPred pred start s = case findIndex pred s of
+getPred :: (Char -> Bool) -> String -> [(Int, String)]
+getPred _ "" = []
+getPred pred s = case findIndex pred s of
   Just n -> let (nums, rest) = span pred (drop n s)
-                nextstart = n + length nums
-            in (start + n, nums) : getPred pred nextstart rest
+            in (n, nums) : getPred pred rest
   Nothing -> []
 
-getNums = getPred isDigit
+fixCols :: [(Int, String)] -> [(Int, String)]
+fixCols pairs = zip new (map snd pairs)
+  where iorig = map fst pairs
+        lens = map (\ (_,num) -> length num) pairs
+        new = scanl1 (+) $ head iorig : zipWith (+) (tail iorig) lens
+
+getNums = fixCols . getPred isDigit
 isNotNumDot x = not (isDigit x) && (x /= '.')
-getSymbols = getPred isNotNumDot
+getSymbols = fixCols . getPred isNotNumDot
 makeMap = Map.fromList . concat . zipWith (\ r stuff -> map (\ (c, v) -> ((r, c), v)) stuff) [0..]
 
 getValue :: ((Int,Int), String) -> Map (Int,Int) a -> Int
@@ -37,11 +42,7 @@ main :: IO ()
 main = do
   input <- readFile "2023/03/input"
   let contents = lines input
-      lnums = makeMap $ map (getNums 0) contents
-      lsyms = makeMap $ map (getSymbols 0) contents
+      lnums = makeMap $ map getNums contents
+      lsyms = makeMap $ map getSymbols contents
       result = sum $ map (`getValue` lsyms) (Map.toList lnums)
     in print result
-
-line = head contents
-getNums 0 line
-pred = isDigit
