7d841ed3c6c105ea5814b385205269f3919ec980
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2025-10-10 22:04:33 -0400

    2023.6.2 format

--- 2023/06/solve2.hs
+++ 2023/06/solve2.hs
@@ -1,15 +1,16 @@
 import System.IO
 
-import Data.Text (Text)
-import qualified Data.Text as Text
-import Data.Text.Read (decimal)
-import Data.Either (fromRight)
 import Data.Char (isDigit)
+import Data.Either (fromRight)
+import Data.Text (Text)
+import Data.Text qualified as Text
+import Data.Text.Read (decimal)
 
 readNums :: Text -> Int
-readNums s = fst $ fromRight (0,Text.singleton ' ') $ decimal numstring
-  where numpart = Text.dropWhile (not . isDigit) s
-        numstring = Text.concat $ Text.words numpart
+readNums s = fst $ fromRight (0, Text.singleton ' ') $ decimal numstring
+ where
+  numpart = Text.dropWhile (not . isDigit) s
+  numstring = Text.concat $ Text.words numpart
 
 -- time = push + go
 -- dist = push * go
@@ -18,16 +19,18 @@ readNums s = fst $ fromRight (0,Text.singleton ' ') $ decimal numstring
 -- push^2 - push*time + dist = 0
 quadSolve :: (Int, Int) -> (Double, Double)
 quadSolve (time, dist) = (minus, plus)
-  where b4ac = sqrt(b^2 - 4*c)
-        minus = (-b - b4ac) / 2
-        plus = (-b + b4ac) / 2
-        b = fromIntegral (-time) :: Double
-        c = fromIntegral dist :: Double
+ where
+  b4ac = sqrt (b ^ 2 - 4 * c)
+  minus = (-b - b4ac) / 2
+  plus = (-b + b4ac) / 2
+  b = fromIntegral (-time) :: Double
+  c = fromIntegral dist :: Double
 
 nSolutions :: (Double, Double) -> Int
 nSolutions (a, b) = b' - a'
-  where a' = floor a
-        b' = floor b
+ where
+  a' = floor a
+  b' = floor b
 
 main = do
   input <- readFile "2023/06/input"
@@ -35,4 +38,4 @@ main = do
       inputs = (readNums timestr, readNums diststr)
       quads = quadSolve inputs
       solutions = nSolutions quads
-    in print solutions
+   in print solutions
